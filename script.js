a60b19a51684ddd8addff16d4b3806044241bcf9
Hai Nguyen <memyselfandhai@gmail.com>
2017-10-17 14:09:32 -0500

    refactored step 1

--- script.js
+++ script.js
@@ -1,70 +1,51 @@
-// var text_validate = function(input, max_chars, counter){
-//   var input_length = input.val().length;
-//   var chars_remaining = max_chars - input.length;
-//   if (input.length > 0) {
-//     counter.text(chars_remaining).show();
-//   } else {
-//     counter.hide();
-//   }
-// };
+"use strict";
 
+// length validator
+var text_validate = function(input, max_chars, counter) {
+    input.on("keyup", function () {
+    var input_length = input.val().length;
+    var chars_remaining = max_chars - input.length;
+    if (input.length > 0) {
+      counter.text(chars_remaining).show();
+    } else {
+      counter.hide();
+    }
+})};
 
-$("#text").on("keyup", function() {
-  var input_length = $(this).val().length;
-  var chars_remaining = 32 - input_length;
-  if (input_length > 0){
-    $("#text-counter").text(chars_remaining).show();
-  } else {
-    $("#text-counter").hide();
-  }
-});
-
-$("#textarea").on("keyup", function() {
-  var input_length = $(this).val().length;
-  var chars_remaining = 140 - input_length;
-  if (input_length > 0){
-    $("#textarea-counter").text(chars_remaining).show();
-  } else {
-    $("#textarea-counter").hide();
-  }
-});
-
-$("#password").on("keyup", function() {
-  var input_length = $(this).val().length;
-  var chars_remaining = 16 - input_length;
-  if (input_length > 0){
-    $("#pw-counter").text(chars_remaining).show();
-  } else {
-    $("#pw-counter").hide();
-  }
-});
-
-$("#confirm-password").on("keyup", function() {
-  var input_length = $(this).val().length;
-  var chars_remaining = 16 - input_length;
-  if (input_length > 0){
-    $("#pw-confirm-counter").text(chars_remaining).show();
-  } else {
-    $("#pw-confirm-counter").hide();
-  }
-});
+text_validate( $("#text"), 32, $("#text-counter"))
+text_validate( $("#textarea"), 140, $("#textarea-counter"))
+text_validate( $("#password"), 16, $("#pw-counter"))
+text_validate( $("#confirm-password"), 16, $("#pw-confirm-counter"))
 
 
 
-// var pass_validate = function (initial, confirm) {
-//   if (confirm.length > 0) {
-//     if (initial !== confirm) {
-//       //display "Your passwords do not match." }
-//   }
-// };
+// error messages
+$('.error').hide();
 
-$('#pw-error').hide();
-
+// password confirmation error
 $("#confirm-password").on("keyup", function() {
   if  ( $('#password').val() !== $('#confirm-password').val() &&
         $('#confirm-password').val().length > 0) {
-      $('#pw-error').show();
+      $('#pw-confirm-error').show();
 } else {
-      $('#pw-error').toggle()
-}
-});
+      $('#pw-confirm-error').hide()
+}});
+
+// submit button length error messages
+
+var button_validate = function (input, min_chars, err_message) {
+  $('#submit').click (function (event) {
+    if (input.val().length < min_chars) {
+        event.preventDefault();
+        input.css("border-color", "red");
+        err_message.show();
+    } else {
+      input.css("border-color","initial");
+      err_message.hide();
+    }
+} )} ;
+
+button_validate( $('#password'), 6, $('#pw-error') );
+button_validate( $('#confirm-password'), 6, $('#pw-confirm-error') );
+button_validate( $('#text'), 4, $('#text-error') );
+button_validate( $('#textarea'), 4, $('#textarea-error') );
